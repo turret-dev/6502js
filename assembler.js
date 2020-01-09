a16b4baf386f1a9a2d63863c5817e93979f0750f
David Humphrey <david.humphrey@senecacollege.ca>
2020-01-08 21:55:31 -0500

    Switch from setInterval to requestAnimationFrame

--- assembler.js
+++ assembler.js
@@ -378,7 +378,6 @@ function SimulatorWidget(node) {
     var codeRunning = false;
     var debug = false;
     var monitoring = false;
-    var executeId;
 
     // Set zero and negative processor flags based on result
     function setNVflags(value) {
@@ -1652,7 +1651,7 @@ function SimulatorWidget(node) {
       } else {
         ui.play();
         codeRunning = true;
-        executeId = setInterval(multiExecute, 15);
+        window.requestAnimationFrame(multiExecute);
       }
     }
 
@@ -1665,6 +1664,10 @@ function SimulatorWidget(node) {
         }
       }
       updateDebugInfo();
+
+      if(codeRunning) {
+        window.requestAnimationFrame(multiExecute);
+      }
     }
 
 
@@ -1814,7 +1817,6 @@ function SimulatorWidget(node) {
 
     function stop() {
       codeRunning = false;
-      clearInterval(executeId);
       message("\nStopped\n");
     }
 
